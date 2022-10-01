8684c235c50f49b478473ad7abc47d06ef3e8472
jstnfrncis <107698339+jstnfrncis@users.noreply.github.com>
2022-10-01 19:23:09 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,84 @@
+
+var form = document.getElementById("my-form");
+    
+    async function handleSubmit(event) {
+      event.preventDefault();
+      var status = document.getElementById("status");
+      var data = new FormData(event.target);
+      fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+      }).then(response => {
+        if (response.ok) {
+          status.innerHTML = "Thanks for your message!";
+          form.reset()
+        } else {
+          response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+              status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+              status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+          })
+        }
+      }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+      });
+    }
+    form.addEventListener("submit", handleSubmit)
+
+//     const menu_btn = document.querySelector('.hamburger');
+//     const mobile_menu = document.querySelector('.mobile-nav');
+
+//     menu_btn.addEventListener('click', function(){
+//       menu_btn.classList.toggle('is-active');
+//       mobile_menu.classList.toggle('is-active');
+//     });
+
+//     const menuItems = document.getElementsByClassName("menu-item");
+
+//   for (let i = 0; i < menuItems.length; i++) {
+//   const item = menuItems[i];
+//   item.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     menu_btn.addEventListener('click', function(){
+//       menu_btn.classList.toggle('is-active');
+//       mobile_menu.classList.toggle('is-active');
+//     });
+//   })
+// }
+
+/*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu'),
+      navToggle = document.getElementById('nav-toggle'),
+      navClose = document.getElementById('nav-close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', () =>{
+        navMenu.classList.add('show-menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click', () =>{
+        navMenu.classList.remove('show-menu')
+    })
+}
+
+/*=============== REMOVE MENU MOBILE ===============*/
+const navLink = document.querySelectorAll('.nav__link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+    
